bbc7a727a515912e8b7697e2f3c8d93ef454cb89
luisgustavogd <85771127+luisgustavogd@users.noreply.github.com>
2026-08-03 19:37:32 -0300

    chore: update offline cache

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('rotina-csv-v2').then(cache => cache.addAll(['./','./index.html','./rotina.csv','./manifest.webmanifest'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+const C='travel-intelligence-v1',A=['./','./index.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
